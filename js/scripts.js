0b5bac7d4bfd8c16a65896f020c669f37400b168
tiara amalina salsabila <tiaramalinasalsabila@gmail.com>
2024-05-17 18:52:27 +0800

    update responsive & interactive

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,34 @@ function openSidebar() {
 }
 
 function closeSidebar() {
-  if (!sidebarOpen) {
+  if (sidebarOpen) {
+    // Ubah kondisi menjadi if (sidebarOpen)
     sidebar.classList.remove('sidebar-responsive');
     sidebarOpen = false;
   }
 }
+
+// DROPDOWN
+
+function toggleDropdown(event) {
+  document.getElementById('dropdownMenu').classList.toggle('show');
+  event.stopPropagation(); // Prevent the click from closing the dropdown
+}
+
+function closeDropdown() {
+  var dropdowns = document.getElementsByClassName('dropdown-content');
+  for (var i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+
+// Close the dropdown if clicked outside
+document.addEventListener('click', function (event) {
+  var isClickInsideDropdown = document.getElementById('dropdownMenu').contains(event.target);
+  if (!isClickInsideDropdown) {
+    closeDropdown();
+  }
+});
